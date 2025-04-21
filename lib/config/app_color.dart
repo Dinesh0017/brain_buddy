936cad4e0ba0338abed45de5909b0cc2b24079ae
Dinesh Rathnasiri <dineshrathnasiri017@gmail.com>
2025-04-22 01:01:13 +0530

    add features cards

--- lib/config/app_color.dart
+++ lib/config/app_color.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const Color primary = Color(0xFF2C0014);
-  static const Color secondary = Color(0x00F90063);
+  static const Color secondary = Color(0xFFF90063);
   static const Color background = Color(0xFFFFFFFF);
   static const Color textPrimary = Color(0xFFFFFFFF);
   static const Color textSecondary = Color(0xFF757575);
