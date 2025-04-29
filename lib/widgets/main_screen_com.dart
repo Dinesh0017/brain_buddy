674bb34223bbb34010ee3f633cedfa9b50353684
Dinesh Rathnasiri <dineshrathnasiri017@gmail.com>
2025-04-29 07:40:10 +0530

    bottom navigator routes

--- lib/widgets/main_screen_com.dart
+++ lib/widgets/main_screen_com.dart
@@ -1,10 +1,46 @@
 import 'package:brain_buddy/config/app_color.dart';
-import 'package:flutter/material.dart';
+import 'package:brain_buddy/screens/favorites/favorites_screen.dart';
+import 'package:brain_buddy/screens/home/home_screen.dart';
+import 'package:brain_buddy/screens/notifications/notifications_screen.dart';
+import 'package:brain_buddy/screens/planner/planner_screen.dart';
 
-class MainScreenCom extends StatelessWidget {
+import 'package:flutter/material.dart';
+class MainScreenCom extends StatefulWidget {
   final List<Widget> children;
 
   const MainScreenCom({super.key, required this.children});
+
+  @override
+  _MainScreenComState createState() => _MainScreenComState();
+}
+
+class _MainScreenComState extends State<MainScreenCom> {
+  int _currentIndex = 0; // Track the selected index
+
+  // Method to navigate to the respective page based on the index
+  void _onItemTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+
+    // Navigate to different screens based on the index
+    switch (index) {
+      case 0:
+        Navigator.push(context, MaterialPageRoute(builder: (_) => HomeScreen()));
+        break;
+      case 1:
+        Navigator.push(context, MaterialPageRoute(builder: (_) => NotificationsScreen()));
+        break;
+      case 2:
+        Navigator.push(context, MaterialPageRoute(builder: (_) => PlannerScreen()));
+        break;
+      case 3:
+        Navigator.push(context, MaterialPageRoute(builder: (_) => FavoritesScreen()));
+        break;
+      default:
+        break;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -44,7 +80,7 @@ class MainScreenCom extends StatelessWidget {
         padding: const EdgeInsets.symmetric(horizontal: 16),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: children,
+          children: widget.children,
         ),
       ),
       bottomNavigationBar: Padding(
@@ -60,6 +96,8 @@ class MainScreenCom extends StatelessWidget {
               showSelectedLabels: false,
               showUnselectedLabels: false,
               type: BottomNavigationBarType.fixed,
+              currentIndex: _currentIndex,
+              onTap: _onItemTapped, // Handle item tap
               items: const [
                 BottomNavigationBarItem(
                   icon: Icon(Icons.home_max_rounded, size: 40),
@@ -85,3 +123,4 @@ class MainScreenCom extends StatelessWidget {
     );
   }
 }
+
